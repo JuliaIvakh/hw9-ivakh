cb32e778bd7dda72623ba6bf9fb3680e9cc7082b
JuliaIvakh <85705848+JuliaIvakh@users.noreply.github.com>
2021-10-09 02:55:53 +0300

    hw9

---
+++ js/script.js
@@ -0,0 +1,61 @@
+//Task 1
+let x = 10,
+  y = 7;
+let result = x > y ? "x больше, чем y" : "x не больше, чем y";
+alert(result);
+
+//Task 2
+// let num = +prompt("Enter any number");
+// if (num % 2 === 0) {
+//   alert(`Number ${num} is even`);
+// } else {
+//   alert(`Number ${num} is odd`);
+// }
+
+//Task 3
+// let sign;
+// let type;
+// let numLen;
+// let Len;
+// let num = +prompt("Введи целое число c знаком + или -");
+// let a = `${num}`;
+// if (Math.sign(num) === +1) {
+//   sign = `Число ${num} однозначно положительно`;
+//   numLen = a.length;
+// } else {
+//   sign = `Число ${num} однозначно отрицательное`;
+//   numLen = a.length - 1;
+// }
+// console.log(numLen);
+// if (numLen === 1) {
+//   Len = "Число однозначное";
+// } else if (numLen === 2) {
+//   Len = "Число двузначное";
+// } else if (numLen === 3) {
+//   Len = "Число трехзначное";
+// } else {
+//   Len = "Число многозначное";
+// }
+// alert(`${sign} \n${Len}`);
+
+//Task 4
+// let num1 = prompt("Enter first number");
+// let num2 = prompt("Enter second number");
+// let num3 = prompt("Enter third number");
+// if (num1 > num2 && num1 > num3) {
+//   alert(num1);
+// } else if (num2 > num1 && num2 > num3) {
+//   alert(num2);
+// } else {
+//   alert(num3);
+// }
+
+//Task 5
+// let num1 = prompt("Enter first number");
+// let num2 = prompt("Enter second number");
+// let num3 = prompt("Enter third number");
+// if (num1 + num2 > num3 && num2 + num3 > num1 && num1 + num3 > num2) {
+//   alert("Triangle can exist");
+// } else {
+//   alert("Triangle can not exist");
+// }
